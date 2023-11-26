d4d8f3191d92c72dcd9866bcbd811d9e2804ec1c
ZANGZANGS <kimjangh94@gmail.com>
2023-11-26 21:51:30 +0900

    FEAT: 조인-on절

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -301,4 +301,49 @@ public class QuerydslBasicTest {
                 .containsExactly("teamA", "teamB");
     }
 
+    @DisplayName("회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회")
+    /**
+     * JPAL: select m,t from MEmber m left join m.team t on t.name = 'teamA'
+     */
+
+    @Test
+    public void join_on_filtering(){
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team)
+                .on(team.name.eq("teamA"))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = "+ tuple);
+        }
+
+    }
+
+    /**
+     *세타 조인
+     * 회원의 이름이 팀 이름과 같은 회원 조회
+     */
+    @Test
+    public void join_on_no_relration(){
+        //given
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+        //when
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team).on(member.username.eq(team.name))
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        //then
+        for (Tuple tuple : result) {
+            System.out.println("tuple = "+ tuple);
+        }
+
+    }
 }
