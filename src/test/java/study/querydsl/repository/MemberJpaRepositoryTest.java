751b14894e64a34796d277a062ae034037ac3a86
zangzangs <kimjangh94@gmail.com>
2023-11-30 23:46:29 +0900

    FEAT: 동적 쿼리와 성능 최적화 조회 - Where절 파라미터 사용

--- src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
@@ -81,6 +82,42 @@ class MemberJpaRepositoryTest {
          * 기본 디폴트 조건을 넣어주거나, 페이징을 하는게 안전하다.
          */
         List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member4");
+
+    }
+
+    @Test
+    public void searchTestWhere(){
+        //given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        /**
+         * 불린빌더에 조건이 없으면 where문 없이 쿼리가 수행된다. (대참사가 발생할 수도 있다.)
+         * 기본 디폴트 조건을 넣어주거나, 페이징을 하는게 안전하다.
+         */
+        List<MemberTeamDto> result = memberJpaRepository.serach(condition);
 
         assertThat(result)
                 .extracting("username")
