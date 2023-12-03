e28333edaa35eb5b09d92eb3ac16acce75a2dca4
zangzangs <kimjangh94@gmail.com>
2023-12-03 22:25:45 +0900

    FEAT: 스프링 데이터 페이징 활용1 - Querydsl 페이징 연동

--- src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
@@ -1,8 +1,12 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 import study.querydsl.dto.MemberSearchCondition;
@@ -39,6 +43,70 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
                 )
                 .fetch();
 
+    }
+
+    @Override
+    public Page<MemberTeamDto> searchSimple(MemberSearchCondition condition, Pageable pageable) {
+        QueryResults<MemberTeamDto> memberTeamDtoQueryResults = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();
+
+        List<MemberTeamDto> content = memberTeamDtoQueryResults.getResults();
+        long total = memberTeamDtoQueryResults.getTotal();
+
+        return new PageImpl<>(content,pageable,total);
+    }
+
+    @Override
+    public Page<MemberTeamDto> searchComplex(MemberSearchCondition condition, Pageable pageable) {
+        List<MemberTeamDto> content = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        long total = queryFactory
+                .select(member)
+                .from(member)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .fetch().size();
+
+        return new PageImpl<>(content,pageable,total);
     }
 
 
--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -6,6 +6,9 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
@@ -76,6 +79,44 @@ class MemberRepositoryTest {
         assertThat(result)
                 .extracting("username")
                 .containsExactly("member4");
+
+    }
+
+
+    @Test
+    public void searchPageSimple(){
+        //given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+//        condition.setAgeGoe(35);
+//        condition.setAgeLoe(40);
+//        condition.setTeamName("teamB");
+
+        /**
+         * 불린빌더에 조건이 없으면 where문 없이 쿼리가 수행된다. (대참사가 발생할 수도 있다.)
+         * 기본 디폴트 조건을 넣어주거나, 페이징을 하는게 안전하다.
+         */
+        PageRequest pageRequest = PageRequest.of(0, 3);
+        Page<MemberTeamDto> result = memberRepository.searchSimple(condition, pageRequest);
+
+
+        assertThat(result.getSize()).isEqualTo(3);
+        assertThat(result.getContent()).extracting("username").containsExactly("member1","member2","member3");
 
     }
 }
