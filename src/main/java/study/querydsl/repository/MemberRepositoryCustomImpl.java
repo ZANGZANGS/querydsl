ad0c758e4e41d887e38f4ba5beb83bf420566634
ZANGZANGS <kimjangh94@gmail.com>
2023-12-05 23:16:14 +0900

    FEAT: 리포지토리 지원 - QuerydslRepositorySupport

--- src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
@@ -2,12 +2,15 @@ package study.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -17,17 +20,45 @@ import study.querydsl.dto.QMemberTeamDto;
 import study.querydsl.entity.Member;
 
 import java.util.List;
+import java.util.Objects;
 
 import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
 
 
-@RequiredArgsConstructor
-public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
+//@RequiredArgsConstructor
+public class MemberRepositoryCustomImpl extends QuerydslRepositorySupport implements MemberRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
+
+
+    public MemberRepositoryCustomImpl(EntityManager em) {
+
+        super(Member.class);
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
     @Override
     public List<MemberTeamDto> search(MemberSearchCondition condition) {
+
+        EntityManager em = getEntityManager();
+        
+        from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                ).select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                )).fetch();
+                
+        
         return queryFactory
                 .select(new QMemberTeamDto(
                         member.id.as("memberId"),
@@ -69,6 +100,35 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetchResults();
+
+        List<MemberTeamDto> content = memberTeamDtoQueryResults.getResults();
+        long total = memberTeamDtoQueryResults.getTotal();
+
+        return new PageImpl<>(content,pageable,total);
+    }
+
+
+    public Page<MemberTeamDto> searchSimple2(MemberSearchCondition condition, Pageable pageable) {
+
+        JPQLQuery<MemberTeamDto> jpaQuery = from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                ).select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ));
+
+
+        //메서드 체인이 끊기는 단점이 있다.
+        JPQLQuery<MemberTeamDto> query = Objects.requireNonNull(getQuerydsl()).applyPagination(pageable, jpaQuery);
+        QueryResults<MemberTeamDto> memberTeamDtoQueryResults = query.fetchResults();
 
         List<MemberTeamDto> content = memberTeamDtoQueryResults.getResults();
         long total = memberTeamDtoQueryResults.getTotal();
